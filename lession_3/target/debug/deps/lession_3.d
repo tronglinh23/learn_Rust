fdcd431dfb8325160c3f956f927d757e84121618
tronglinh23 <tronglinh230203@gmail.com>
2023-08-16 11:41:22 +0700

    fix spelling error of folder's name

--- lession_3/target/debug/deps/lession_3.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Admin\Desktop\learn-rust\lession_3\target\debug\deps\lession_3.exe: src\main.rs
-
-C:\Users\Admin\Desktop\learn-rust\lession_3\target\debug\deps\lession_3.d: src\main.rs
-
-src\main.rs:
--- lession_3/target/debug/lession_3.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Admin\Desktop\learn-rust\lession_3\target\debug\lession_3.exe: C:\Users\Admin\Desktop\learn-rust\lession_3\src\main.rs
